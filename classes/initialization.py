7498151406d54058af11f566a69f45787daa4053
Yago Boleas <yagobf.ybf@gmail.com>
2025-12-21 19:49:55 +0100

    [new] Added RandomInitialization class for uniform population initialization

--- classes/initialization.py
+++ classes/initialization.py
@@ -32,3 +32,28 @@ class Initialization(ABC):
         self, population_size: int, bounds: List[Tuple[float, float]], problem: Problem
     ) -> Population:
         return self.initialize(population_size, bounds, problem)
+
+
+class RandomInitialization(Initialization):
+    """Random uniform initialization within bounds."""
+
+    def initialize(
+        self,
+        population_size: int,
+        bounds: List[Tuple[float, float]],
+        problem: Problem,
+    ) -> Population:
+        """
+        Create random population.
+
+        :param population_size: Number of individuals
+        :type population_size: int
+        :param bounds: Variable bounds
+        :type bounds: List[Tuple[float, float]]
+        :param problem: Problem instance
+        :type problem: Problem
+        :return: Random population
+        :rtype: Population
+        """
+        individuals = [RealIndividual(bounds=bounds) for _ in range(population_size)]
+        return Population(individuals, minimize=True)
