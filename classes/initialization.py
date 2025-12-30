c1b43498510d4e51ebe8af29879c0c489b8a5cf5
Yago Boleas <yagobf.ybf@gmail.com>
2025-12-30 16:41:50 +0100

    [new] Added permutation initialization

--- classes/initialization.py
+++ classes/initialization.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import List, Tuple
 
-from .individual import RealIndividual
+from .individual import PermutationIndividual, RealIndividual
 from .population import Population
 from .problems import Problem
 
@@ -11,7 +11,7 @@ class Initialization(ABC):
 
     @abstractmethod
     def initialize(
-        self, population_size: int, bounds: List[Tuple[float, float]], problem: Problem
+        self, population_size: int, bounds: List, problem: Problem
     ) -> Population:
         """
         Initialize population.
@@ -19,7 +19,7 @@ class Initialization(ABC):
         :param population_size: Number of individuals
         :type population_size: int
         :param bounds: Variable bounds
-        :type bounds: List[Tuple[float, float]]
+        :type bounds: List
         :param problem: Problem instance for evaluation tracking
         :type problem: Problem
         :return: Initialized population
@@ -47,11 +47,34 @@ class RandomInitialization(Initialization):
         :param population_size: Number of individuals
         :type population_size: int
         :param bounds: Variable bounds
-        :type bounds: List[Tuple[float, float]]
+        :type bounds: List
         :param problem: Problem instance
         :type problem: Problem
         :return: Random population
         :rtype: Population
         """
         individuals = [RealIndividual(bounds=bounds) for _ in range(population_size)]
+        return Population(individuals, minimize=True)
+
+
+class PermutationInitialization(Initialization):
+    """Random permutation initialization."""
+
+    def initialize(
+        self,
+        population_size: int,
+        bounds: Tuple[int, int],
+        problem: Problem,
+    ) -> Population:
+        """
+        Create a population of individuals with permuted genotypes.
+
+        :param population_size: Number of individuals to create.
+        :param bounds: Tuple defining the range of integers (min, max).
+        :param problem: Problem instance.
+        :return: Population of PermutationIndividuals.
+        """
+        individuals = [
+            PermutationIndividual(bounds=bounds) for _ in range(population_size)
+        ]
         return Population(individuals, minimize=True)
